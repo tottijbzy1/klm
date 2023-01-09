c9ea1c8f90fc1cdd968d89ea5b067782f847c97d
tottijbzy <33811470+tottijbzy1@users.noreply.github.com>
2023-01-09 23:23:52 +0200

    Add files via upload

---
+++ iframe rediect test1111111111111111.js
@@ -0,0 +1,10 @@
+/**
+ * Created by sposmen on 29/09/14.
+ */
+iframe = document.getElementById('deeptarget');
+//TODO: Depends on each providers structure and
+iframe.src = 'https://divlancer.com/9779?slug=%D8%AE%D9%85%D8%B3-%D8%A3%D9%81%D9%83%D8%A7%D8%B1-%D9%85%D8%B4%D8%A7%D8%B1%D9%8A%D8%B9-%D8%AA%D8%AC%D8%A7%D8%B1%D9%8A%D8%A9-%D8%A7%D9%88%D9%84%D8%A7%D9%8A%D9%86-%D9%84%D9%83%D8%B3%D8%A8-%D8%A7%D9%84%D9%85%D8%A7%D9%84-%D9%85%D9%86-%D8%A7%D9%84%D8%A7%D9%86%D8%AA%D8%B1%D9%86%D8%AA-%D9%84%D9%84%D9%85%D8%A8%D8%AA%D8%AF%D8%A6%D9%8A%D9%86-%D8%A8%D8%B3%D8%B1%D8%B9%D8%A9-%D9%81%D9%8A-%D8%B9%D8%A7%D9%85-2023';
+iframe.onload = function (){
+
+  window.location.replace('https://t.co/WiPUzrSdFU')
+};
